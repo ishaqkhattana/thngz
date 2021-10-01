b04e54e4b35975d1b5f6a51c2a63a5c329b323d3
ishaq-alphasquad <ishaq.alphasquad@gmail.com>
2021-10-01 15:21:33 +0500

    Amplify setup

---
+++ src/graphql/queries.js
@@ -0,0 +1,159 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getOrder = /* GraphQL */ `
+  query GetOrder($id: ID!) {
+    getOrder(id: $id) {
+      id
+      UserID
+      Date
+      Total
+      createdAt
+      updatedAt
+      User {
+        id
+        FirstName
+        LastName
+        Phone
+        Email
+        ContactNumber
+        StreetAddress
+        City
+        State
+        ProfileImage
+        createdAt
+        updatedAt
+        owner
+      }
+      Products {
+        items {
+          id
+          Title
+          Description
+          Image
+          Price
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      customer
+    }
+  }
+`;
+export const listOrders = /* GraphQL */ `
+  query ListOrders(
+    $filter: ModelOrderFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listOrders(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        UserID
+        Date
+        Total
+        createdAt
+        updatedAt
+        User {
+          id
+          FirstName
+          LastName
+          Phone
+          Email
+          ContactNumber
+          StreetAddress
+          City
+          State
+          ProfileImage
+          createdAt
+          updatedAt
+          owner
+        }
+        Products {
+          nextToken
+        }
+        customer
+      }
+      nextToken
+    }
+  }
+`;
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      FirstName
+      LastName
+      Phone
+      Email
+      ContactNumber
+      StreetAddress
+      City
+      State
+      ProfileImage
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        FirstName
+        LastName
+        Phone
+        Email
+        ContactNumber
+        StreetAddress
+        City
+        State
+        ProfileImage
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
+export const getProduct = /* GraphQL */ `
+  query GetProduct($id: ID!) {
+    getProduct(id: $id) {
+      id
+      Title
+      Description
+      Image
+      Price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listProducts = /* GraphQL */ `
+  query ListProducts(
+    $filter: ModelProductFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listProducts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        Title
+        Description
+        Image
+        Price
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
